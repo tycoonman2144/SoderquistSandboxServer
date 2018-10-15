ba303acdb83dc078b8b81a9719f478dab5c7624b
psoder3 <paulsoderquist3@gmail.com>
2018-10-14 20:29:34 -0600

    Add files via upload

---
+++ Back End/server.js
@@ -0,0 +1,20 @@
+// set up server
+var config = require('./config/config.js'), // import config variables
+  port = config.port,                       // set the port
+  express = require('express'),             // use express as the framwork
+  app = express(),                          // create the server using express
+  path = require('path');                   // utility module
+
+app.use(express.static(path.join(__dirname, 'public'))); // this middleware serves static files, such as .js, .img, .css files
+
+// Initialize server
+var server = app.listen(port, function () {
+  console.log('Listening on port %d', server.address().port);
+});
+
+// Use '/' to go to index.html via static middleware
+
+// Use '/test' to send "test" as a response.
+app.get('/test', function (req, res) {
+  res.send('tested');
+});
